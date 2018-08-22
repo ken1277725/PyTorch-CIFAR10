88e62d8ed11cad7a297e7f8f43a87e9a6e793107
YuanChingLin <ken1277725>
2018-08-22 17:37:29 +0800

    try to log image
    as title

--- util/train.py
+++ util/train.py
@@ -29,7 +29,6 @@ MSG_DISPLAY_FREQ = 200
 def train(train_loader, model, criterion, optimizer, epoch, USE_GPU=False,writer=None):
 
     batch_time = 0.0
-
     # switch to train mode
     model.train()
 
@@ -38,7 +37,7 @@ def train(train_loader, model, criterion, optimizer, epoch, USE_GPU=False,writer
     running_loss = 0.0
     total_batch_num = len(train_loader)
     for i, (inputs, labels) in enumerate(train_loader):
-
+        n_iter = total_batch_num * epoch + i
         labels = torch.squeeze(labels, 1)
 
         if USE_GPU:
@@ -58,7 +57,12 @@ def train(train_loader, model, criterion, optimizer, epoch, USE_GPU=False,writer
         batch_time += time.time()-end
         end = time.time()
         if writer != None :
-            writer.add_scalar('data/s1',loss.data[0],total_batch_num * epoch + i)
+            writer.add_scalar('data/s1',loss.data[0],n_iter)
+            if i % MSG_DISPLAY_FREQ == (MSG_DISPLAY_FREQ-1):
+                print("loss_value:{}".format(loss_value.data.item()))
+                
+                out = torch.cat((outputs.data, torch.ones(len(outputs), 1)), 1)
+                writer.add_embedding(out, metadata=labels, label_img=inputs, global_step=n_iter)
         if i % MSG_DISPLAY_FREQ == (MSG_DISPLAY_FREQ-1):
             print("[{}][{}/{}]\t Loss: {:0.5f}\t Batch time: {:0.3f}s".format(epoch, i+1, len(train_loader), running_loss/MSG_DISPLAY_FREQ, batch_time/MSG_DISPLAY_FREQ))
             running_loss = 0.0
